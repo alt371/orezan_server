15ec64a25146260b7e53506f7001c2a70538da88
Leko <leko.noor@gmail.com>
2013-12-26 21:20:36 +0900

    Added index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once "classes/db.php";
+require_once "classes/model/user.php";
+
+echo "<pre>";
+echo "\n\nfind(1)\n";
+var_dump(Model_User::find(1));
+
+echo "\n\nfindAll()\n";
+print_r(Model_User::findAll());
+
+echo "\n\ncreate()\n";
+var_dump(Model_User::create(array(
+    'name' => 'hoge',
+    'password' => 'foo'
+)));
+
+echo "\n\nupdate()\n";
+var_dump(Model_User::update(1, array(
+    'name' => 'Leko'
+)));
+
+echo "\n\ndelete()\n";
+var_dump(Model_User::delete(2));
