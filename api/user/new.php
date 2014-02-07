985369ad7b05a8823a90d5f8208c08f70623e005
Leko <leko.noor@gmail.com>
2014-02-07 17:14:19 +0900

    ユーザ作成時にサンプル出費を自動で作成するよう実装

--- api/user/new.php
+++ api/user/new.php
@@ -2,6 +2,7 @@
 //new.php: ユーザ情報受け取り、そのidのユーザを作成する
 
 require_once dirname(__FILE__).'/../../model/user.php';
+require_once dirname(__FILE__).'/../../model/expenses.php';
 
 $response = array();
 
@@ -12,6 +13,16 @@ if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['balan
         'password' => $_POST['password'],
         'balance' => $_POST['balance']
     ));
+
+    // サンプル出費を追加
+    Model_Expenses::create_expenses(
+        $_SESSION['user_id'],
+        "サンプル",
+        date("Y-m-d H:i:s"),
+        0,
+        0,
+        "出費のサンプルです。このように表示されます。"
+    );
 } else {
     $response['error'] = array(
         'message' => 'Bad Request',
